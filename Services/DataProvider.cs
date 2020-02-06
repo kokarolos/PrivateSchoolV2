fdef1ec0384f4e12b124e17b11bc78ed847faada
kokarolos <56258663+kokarolos@users.noreply.github.com>
2020-02-06 11:12:55 +0200

    Add files via upload

---
+++ Services/DataProvider.cs
@@ -0,0 +1,215 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Services
+{
+    public class DataProvider
+    {
+        private static string connection = ConfigurationManager.ConnectionStrings["PrivateSchool"].ConnectionString;
+        private static readonly List<string> queries = new List<string>()
+        {
+            "Select * from Student", //GetAllStudents
+            "Select * from trainer t inner join Subject s on s.TrainerID = t.id;", //GetAllTrainers
+            "Select * from Assignment a inner join Grade g on g.AssignmentID = a.ID", //GetAllAssignments
+            "Select * from Course", //GetAllCourses
+            "Select sc.ID,c.Title, s.FirstName,s.LastName from Course c inner join StudentCourse sc on sc.CourseID = c.ID inner join Student s on s.ID = sc.StudentID", //GetStudentPerCourses
+            "Select c.Title,t.FirstName, t.LastName from Course c inner join Trainer t on t.ID = c.TrainerID;", //GetTrainerPerCourses
+            "Select c.ID, c.Title,a.Title from Course c inner join Assignment a on a.ID = c.AssignmentID order by c.Id;", //GetAssignmentPerCourses
+            "Select a.Title,c.Title, s.FirstName, s.LastName from Course c inner join StudentCourse sc on sc.CourseID = c.ID" + //GetAssigmentPerCoursesPerStudent
+                    " inner join Student s on s.ID = sc.StudentID inner join Assignment a on a.ID = c.AssignmentID;",
+            "Select s.FirstName , s.LastName,Count(c.ID) as totalCourse from Course c inner join StudentCourse sc on sc.CourseID = c.ID " + //GetStudentCourseCount
+            "inner join Student s on s.ID = sc.StudentID group by s.FirstName , s.LastName having COUNT(c.ID) > 1;"
+        };
+        //Gets All students returning a list<Student>
+        public static List<Student> GetAllStudents()
+        {
+            List<Student> students = new List<Student>();
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(queries[0], con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var id = Convert.ToInt32(reader[0]);
+                    var firstName = reader[1].ToString();
+                    var lastName = reader[2].ToString();
+                    decimal? tuitionFees = string.IsNullOrWhiteSpace(reader[3].ToString()) ? (decimal?)null : Convert.ToDecimal(reader[3]);
+                    DateTime? dateOfBirth = string.IsNullOrWhiteSpace(reader[4].ToString()) ? (DateTime?)null : Convert.ToDateTime(reader[4]);
+                    students.Add(new Student(id, firstName, lastName, tuitionFees, dateOfBirth));
+                }
+            }
+            return students;
+        }
+        //Gets All Trainers returning a list<Trainer>
+        public static List<Trainer> GetAllTrainers()
+        {
+            List<Trainer> trainers = new List<Trainer>();
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(queries[1], con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var id = Convert.ToInt32(reader[0]);
+                    var firstName = reader[1].ToString();
+                    var lastName = reader[2].ToString();
+                    var subject = reader[4].ToString();
+                    trainers.Add(new Trainer(id, firstName, lastName, subject));
+                }
+            }
+            return trainers;
+        }
+        //Gets All ASsignments returning a list<Assignment>
+        public static List<Assignment> GetAllAssignments()
+        {
+            List<Assignment> assignments = new List<Assignment>();
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(queries[2], con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var id = Convert.ToInt32(reader[0]);
+                    var description = string.IsNullOrWhiteSpace(reader[1].ToString()) ? null : reader[1].ToString();
+                    var title = reader[2].ToString();
+                    DateTime? subDate = string.IsNullOrWhiteSpace(reader[3].ToString()) ? (DateTime?)null : Convert.ToDateTime(reader[3]);
+                    var oralMark = Convert.ToSingle(reader[5]);
+                    var totalmark = Convert.ToSingle(reader[6]);
+
+                    assignments.Add(new Assignment(id, description, title, subDate, oralMark, totalmark));
+                }
+            }
+            return assignments;
+        }
+        //Gets All courses returning a list<Course>
+        public static List<Course> GetAllCourses()
+        {
+            List<Course> courses = new List<Course>();
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(queries[3], con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var id = Convert.ToInt32(reader[0]);
+                    var title = reader[1].ToString();
+                    var stream = reader[2].ToString();
+                    var type = reader[3].ToString();
+                    DateTime? startDate = string.IsNullOrWhiteSpace(reader[6].ToString()) ? (DateTime?)null : Convert.ToDateTime(reader[6]);
+                    DateTime? endDate = string.IsNullOrWhiteSpace(reader[7].ToString()) ? (DateTime?)null : Convert.ToDateTime(reader[7]);
+                    courses.Add(new Course(id, title, stream, type, startDate, endDate));
+                }
+            }
+            return courses;
+        }
+        //Gets Students related to course returning a list<StudentCourse>
+        public static List<StudentCourse> GetStudentPerCourses()
+        {
+            List<StudentCourse> studentPerCourses = new List<StudentCourse>();
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(queries[4], con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var id = Convert.ToInt32(reader[0]);
+                    var title = reader[1].ToString();
+                    var firstName = reader[2].ToString();
+                    var lastName = reader[3].ToString();
+
+                    studentPerCourses.Add(new StudentCourse(id, title, firstName, lastName));
+                }
+            }
+            return studentPerCourses;
+        }
+        //Gets Trainers related to course returning a list<TrainerCourse>
+        public static List<TrainerCourse> GetTrainerPerCourses()
+        {
+            List<TrainerCourse> trainerCourses = new List<TrainerCourse>();
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(queries[5], con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var courseTitle = reader[0].ToString();
+                    var firstName = reader[1].ToString();
+                    var lastName = reader[2].ToString();
+
+                    trainerCourses.Add(new TrainerCourse(courseTitle, firstName, lastName));
+                }
+            }
+            return trainerCourses;
+        }
+        //Gets Assignments related to course returning a list<AssignmentCourse>
+        public static List<AssignmentCourse> GetAssignmentPerCourses()
+        {
+            List<AssignmentCourse> assignmentPerCourses = new List<AssignmentCourse>();
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(queries[6], con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var id = Convert.ToInt32(reader[0]);
+                    var courseTitle = reader[1].ToString();
+                    var assignmentTitle = reader[2].ToString();
+
+                    assignmentPerCourses.Add(new AssignmentCourse(id, courseTitle, assignmentTitle));
+                }
+            }
+            return assignmentPerCourses;
+
+        }
+        //Gets Assignments related to a course and Student returning a list<AssignmentCourseStudent>
+        public static List<AssignmentCourseStudent> GetAssigmentPerCoursesPerStudent()
+        {
+            List<AssignmentCourseStudent> assigmentCourseStudents = new List<AssignmentCourseStudent>();
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(queries[7], con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var assignmentTitle = reader[0].ToString();
+                    var courseTitle = reader[1].ToString();
+                    var studentFirstName = reader[2].ToString();
+                    var studentLastName = reader[3].ToString();
+
+                    assigmentCourseStudents.Add(new AssignmentCourseStudent(assignmentTitle, courseTitle, studentFirstName, studentLastName));
+                }
+            }
+            return assigmentCourseStudents;
+        }
+        //Gets Student that has more than one course also returning a list<StudentCourseCount>
+        public static List<StudentCourseCount> GetStudentCourseCount()
+        {
+            List<StudentCourseCount> studentCourseCount = new List<StudentCourseCount>();
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(queries[8], con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var studentFirstName = reader[0].ToString();
+                    var studentLastName = reader[1].ToString();
+                    var countOfCourses = Convert.ToInt32(reader[2]);
+                    studentCourseCount.Add(new StudentCourseCount(studentFirstName, studentLastName, countOfCourses));
+                }
+            }
+            return studentCourseCount;
+        }
+    }
+}
---
+++ Services/Insertion.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Services
+{
+    public class Insertion
+    {
+        private static readonly string connection = ConfigurationManager.ConnectionStrings["PrivateSchool"].ConnectionString;
+        //Method responsible to Insert a student into db 
+        public static void InsertStudent(string firstName, string lastName, decimal? tuitionFees, DateTime? dateOfBirth)
+        {
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                var query = "Insert Into Student(FirstName, LastName, TuitionFees, DateOfBirth) values(@FirstName, @LastName, @TuitionFees, @DateOfBirth)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                //Passing Values To Parameters
+                cmd.Parameters.AddWithValue("@FirstName", firstName);
+                cmd.Parameters.AddWithValue("@LastName", lastName);
+                cmd.Parameters.AddWithValue("@TuitionFees", tuitionFees ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@DateOfBirth", dateOfBirth ?? (object)DBNull.Value);
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Student Inserted Successfully");
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Error Generated. Details: " + e.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+        //Method responsible to Insert a Trainer into db 
+        public static void InsertTrainer(string firstName, string lastName, string subject)
+        {
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                var Trainerquery = "Insert Into Trainer(FirstName, LastName) values(@FirstName, @LastName) SELECT SCOPE_IDENTITY()";
+                var subjectQuery = "Insert into Subject(Title,TrainerID) values(@Title, @TrainerID);";
+                SqlCommand cmd = new SqlCommand(Trainerquery, con);
+
+                //Passing Values To Parameters
+                cmd.Parameters.AddWithValue("@FirstName", firstName);
+                cmd.Parameters.AddWithValue("@LastName", lastName);
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    var insertedId = cmd.ExecuteScalar();
+                    SqlCommand subcmd = new SqlCommand(subjectQuery, con);
+                    subcmd.Parameters.AddWithValue("@TrainerID", Convert.ToInt32(insertedId));
+                    subcmd.Parameters.AddWithValue("@Title", subject);
+                    subcmd.ExecuteNonQuery();
+                    Console.WriteLine("Trainer Inserted Successfully");
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Error Generated. Details: " + e.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+        //Method responsible to Insert an Assignment into db 
+        public static void InsertAssignment(string description, string title, DateTime? subDate, float oralMark, float totalMark)
+        {
+            var gradeQuery = "Insert into Grade(OralMark,TotalMark,AssignmentID) Values(@OralMark,@TotalMark,@AssignmentID)";
+            var assignmentQuery = "Insert Into Assignment(Description, Title, SubDate) values(@Description, @Title, @SubDate) SELECT SCOPE_IDENTITY()";
+
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                SqlCommand cmd = new SqlCommand(assignmentQuery, con);
+
+                //Passing Values To Parameters
+                cmd.Parameters.AddWithValue("@Description", string.IsNullOrWhiteSpace(description) ? description : (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@Title", title);
+                cmd.Parameters.AddWithValue("@SubDate", subDate ?? (object)DBNull.Value);
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    var insertedId = cmd.ExecuteScalar();
+                    SqlCommand gradecmd = new SqlCommand(gradeQuery, con);
+                    gradecmd.Parameters.AddWithValue("@OralMark", oralMark);
+                    gradecmd.Parameters.AddWithValue("@TotalMark", totalMark);
+                    gradecmd.Parameters.AddWithValue("@AssignmentID", insertedId);
+                    gradecmd.ExecuteNonQuery();
+                    Console.WriteLine("Assignment Inserted Successfully");
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Error Generated. Details: " + e.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+        //Method responsible to Insert a Course into db 
+        public static void InsertCourse(string title, string stream, string type, DateTime? startDate, DateTime? endDate)
+        {
+            var courseQuery = "Insert into Course(Title,Stream,Type,StartDate,EndDate) values(@Title,@Stream,@Type,@StartDate,@EndDate)";
+
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                SqlCommand cmd = new SqlCommand(courseQuery, con);
+
+                cmd.Parameters.AddWithValue("@Title", title);
+                cmd.Parameters.AddWithValue("@Stream", stream);
+                cmd.Parameters.AddWithValue("@Type", type);
+                cmd.Parameters.AddWithValue("@StartDate", startDate ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@EndDate", endDate ?? (object)DBNull.Value);
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Course Inserted Successfully");
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Error Generated. Details: " + e.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+
+            }
+        }
+        //Method responsible to Insert a StudentPerCourse into db 
+        public static void InsertStudentsPerCourse(int StudentID, int CourseID)
+        {
+            var studentPerCourseQuery = "Insert into StudentCourse(StudentID,CourseID) values(@StudentID,@CourseID)";
+
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                SqlCommand cmd = new SqlCommand(studentPerCourseQuery, con);
+
+                cmd.Parameters.AddWithValue("@StudentID", StudentID);
+                cmd.Parameters.AddWithValue("@CourseID", CourseID);
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Student Inserted Succesfully into course");
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Error Generated. Details: " + e.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+        //Method responsible to Insert a TrainerPerCourse into db 
+        public static void InsertTrainersPerCourse(int courseID, int trainerID)
+        {
+            var coursequery =
+                "Update Course Set TrainerID = TrainerID where Course.ID = @CourseID;";
+
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                SqlCommand cmd = new SqlCommand(coursequery, con);
+
+                cmd.Parameters.AddWithValue("@TrainerID", trainerID);
+                cmd.Parameters.AddWithValue("@CourseID", courseID);
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Trainer Inserted Succesfully into Course");
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Error Generated. Details: " + e.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+        //Method responsible to Insert an AssignementPerStudentPerCourse into db 
+        public static void InsertAssignmentPerStudentPerCourse(int asssignmentId, int courseId, int studentID)
+        {
+            var CourseAssignmentQuery = " Update Course Set AssignmentID = @AssignmentId where Course.ID = @CID";
+            var StudentCourseQuery = "Insert into StudentCourse(StudentID,CourseID) Values(@StudentID,@CourseID)";
+
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                SqlCommand cmd = new SqlCommand(CourseAssignmentQuery, con);
+
+                //Passing Values To Parameters
+                cmd.Parameters.AddWithValue("@AssignmentId", asssignmentId);
+                cmd.Parameters.AddWithValue("@CID", courseId);
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    SqlCommand scmd = new SqlCommand(StudentCourseQuery, con);
+                    scmd.Parameters.AddWithValue("@StudentID", studentID);
+                    scmd.Parameters.AddWithValue("@CourseID", courseId);
+                    scmd.ExecuteNonQuery();
+                    Console.WriteLine("Assignmnet Per Course Per Student Inserted Successfully");
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Error Generated. Details: " + e.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+
+
+
+
+        }
+
+
+    }
+}
